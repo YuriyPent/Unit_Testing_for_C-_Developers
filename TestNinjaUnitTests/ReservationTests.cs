06540ed1fe9460f7a76ae400c262de8bbbd3afd4
Yuriy <Seaman_on_watch@mail.ru>
2019-10-26 15:39:34 +0300

    Add code

---
+++ TestNinjaUnitTests/ReservationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCanccelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+            // Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCAncellingTheReservation_ReturnTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user};
+            var result = reservation.CanBeCancelledBy(user);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCAncellingTheReservation_ReturnFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User()};
+            var result = reservation.CanBeCancelledBy(new User());
+            Assert.IsFalse(result);
+        }
+    }
+}
