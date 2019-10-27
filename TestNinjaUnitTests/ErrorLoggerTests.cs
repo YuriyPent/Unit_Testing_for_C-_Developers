a256f75fc0ac2587cad37ecf452fc0c27277b11c
Yuriy <Seaman_on_watch@mail.ru>
2019-10-27 18:45:38 +0300

    Testing Methods that Throw Exceptions

--- TestNinjaUnitTests/ErrorLoggerTests.cs
+++ TestNinjaUnitTests/ErrorLoggerTests.cs
@@ -18,5 +18,15 @@ namespace TestNinjaUnitTests
             logger.Log("a");
             Assert.That(logger.LastError, Is.EqualTo("a"));
         }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
     }
 }
